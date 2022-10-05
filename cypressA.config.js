0020d9e62ae06150000c1d91ff3925c056d79af8
Bega <bega@shirins-MacBook-Air.local>
2022-10-05 13:38:38 +0200

    config file changed to default

--- cypressA.config.js
+++ cypressA.config.js
@@ -1,7 +1,23 @@
 const { defineConfig } = require('cypress');
+/**
+ * @type {Cypress.PluginConfig}
+ */
 
 module.exports = defineConfig({
   projectId: 'tzzaac',
+  e2e: {
+    setupNodeEvents(on, config) {
+      //require('cypress-log-to-output').install(on)
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+    },
+  },
   experimentalStudio: true,
   video: true,
   env: {
